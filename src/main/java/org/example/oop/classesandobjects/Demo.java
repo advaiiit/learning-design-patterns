8954ab6cc0f1c7a5fc3933bbf98ef4147aee108c
Advait Kamathe <advaitkamathe@gmail.com>
2023-09-11 23:44:04 +0530

    Adding package classesandobject

---
+++ src/main/java/org/example/oop/classesandobjects/Demo.java
@@ -0,0 +1,19 @@
+package org.example.oop.classesandobjects;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.id = 1;
+        student1.name = "John Doe";
+        student1.email = "johndoe@gmail.com";
+        student1.study();
+
+        Student student2 = new Student(2, "Jane Doe", "janedoe@gmail.com");
+        student2.study();
+
+        Student student3 = new Student(3, "James Doe");
+        System.out.println(student3.id);
+    }
+}
---
+++ src/main/java/org/example/oop/classesandobjects/Student.java
@@ -0,0 +1,35 @@
+package org.example.oop.classesandobjects;
+
+public class Student {
+
+    int id;
+    String name;
+    String email;
+
+    // Non-parameterized constructor
+    public Student() {}
+
+    // Parameterized constructor
+    public Student(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // this refers current object
+    // this() is used for calling class constructor from different
+    // constructor of the same class
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public Student(int id, String name) {
+        this(id);
+        this.name = name;
+    }
+
+
+    public void study() {
+        System.out.println(name + " is studying");
+    }
+}
